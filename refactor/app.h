7b694bf819addc4f43029fe0ff3054feb6626c85
ipon1207 <ipon_1207@yahoo.co.jp>
2025-09-02 18:05:55 +0900

    fix: app.h

--- refactor/app.h
+++ refactor/app.h
@@ -1,3 +1,7 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "spikeapi.h"
 
 #define MAIN_PRIORITY (TMIN_APP_TPRI + 1)
@@ -7,6 +11,14 @@
 #define STACK_SIZE (4096)
 #endif /* STACK_SIZE */
 
+#ifndef TOPPERS_MACRO_ONLY
+
 extern void main_task(intptr_t exinf);
 extern void tracer_task(intptr_t exinf);
 extern void tracer_cyc(intptr_t exinf);
+
+#endif /* TOPPERS_MACRO_ONLY */
+
+#ifdef __cplusplus
+}
+#endif
